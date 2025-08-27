3842bdc2de6400f8bc6e9a65b801028c69cf98f9
SayanMagar2012 <639405@student.dmschools.org>
2025-08-27 15:18:38 -0500

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,133 @@
+// Here all versions are located, the same as app.js but this time its apps.js because all versions info is located in this one file
+// To add more versions, copy the versions.v1 and replace with versions.v2, and the same with others (it support more than 9 versions)
+
+versions.v1 = {
+  name: "Template",
+  version: "temp01",
+  date: "2025",
+  folder: "asset-v1/",
+  ambience: "",
+  looptime: 9600,
+  bpm: 100,
+  totalframe: 460,
+  nbpolo: 7,
+  maxrecloop: 24,
+  bonusloopA: false, 
+  bonusendloopA: true,
+  colBck: "#0F0F0F",
+  col0: "#919191",
+  col1: "#8A72FF",
+  col2: "#5E49C6",
+  col3: "#2F217A",
+  col4: "#0D043A",
+  animearray: [
+    {
+      name: "1_pump",
+      color: "D8711D",
+      uniqsnd: !0,
+    },
+    {
+      name: "2_wheel",
+      color: "D8711D",
+      uniqsnd: !0,
+    },
+    {
+      name: "3_tiktuk",
+      color: "D8711D",
+      uniqsnd: !0,
+    },
+    {
+      name: "4_cluck",
+      color: "D8711D",
+      uniqsnd: !0,
+    },
+    {
+      name: "5_hammer",
+      color: "D8711D",
+      uniqsnd: !0,
+    },
+    {
+      name: "6_bass",
+      color: "4043CC",
+      uniqsnd: !0,
+    },
+    {
+      name: "7_cube",
+      color: "4043CC",
+      uniqsnd: !0,
+    },
+    {
+      name: "8_xd",
+      color: "4043CC",
+      uniqsnd: !0,
+    },
+    {
+      name: "9_engineer",
+      color: "4043CC",
+      uniqsnd: !0,
+    },
+    {
+      name: "10_computer",
+      color: "4043CC",
+      uniqsnd: !0,
+    },
+    {
+      name: "11_alert",
+      color: "10E929",
+      uniqsnd: !0,
+    },
+    {
+      name: "12_synth",
+      color: "10E929",
+      uniqsnd: !0,
+    },
+    {
+      name: "13_beep",
+      color: "10E929",
+      uniqsnd: !0,
+    },
+    {
+      name: "14_old_alert",
+      color: "10E929",
+      uniqsnd: !0,
+    },
+    {
+      name: "15_whistle",
+      color: "10E929",
+      uniqsnd: !0,
+    },
+    {
+      name: "16_electric",
+      color: "E5181F",
+      uniqsnd: !0,
+    },
+    {
+      name: "17_bitbuzz",
+      color: "E5181F",
+      uniqsnd: !0,
+    },
+    {
+      name: "18_forces",
+      color: "E5181F",
+      uniqsnd: !0,
+    },
+    {
+      name: "19_shady",
+      color: "E5181F",
+      uniqsnd: !0,
+    },
+    {
+      name: "20_XDDDD",
+      color: "E5181F",
+      uniqsnd: !0,
+    },
+  ],
+  bonusarray: [],
+
+};
+
+// versions.v2 = {
+//  name: "New Version",
+// version: "2",
+// date: "2025",
+// ...
